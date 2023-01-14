802d4b6e672fe8b9a1f8b247df4496f765854168
TimSon777 <dusalimov.tima@yandex.ru>
2023-01-14 19:10:29 +0300

    fix

--- src/server/Application/Queries/CurrentGames/Models/GameItem.cs
+++ src/server/Application/Queries/CurrentGames/Models/GameItem.cs
@@ -4,6 +4,6 @@ public sealed class GameItem
 {
     public required string UserName { get; set; }
     public required int Id { get; set; }
-    public required DateTime CreatedDateTimeUtc { get; set; }
+    public required DateTimeOffset CreatedDateTimeUtc { get; set; }
     public required bool IsAvailableToJoin { get; set; }
 }
